38b285892cf4dd3961a7e8570189b5b4862c40c2
SAHA-YJB <pkomom123@naver.com>
2023-12-13 09:31:17 +0900

    comment: soket / jsmpeg connect

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useEffect, useRef } from 'react';
 
+// 이 라이브러리는 MPEG1 동영상을 디코딩하는데 사용
 const jsmpeg = require('jsmpeg');
 
 interface VideoCardProps {
@@ -9,12 +10,18 @@ interface VideoCardProps {
 }
 
 function VideoCard({ url, cameraName }: VideoCardProps) {
+  // 'canvasRef'는 캔버스 요소에 대한 참조를 저장
+  // 'playerRef'는 'jsmpeg' 플레이어 인스턴스에 대한 참조를 저장
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const playerRef = useRef<any>(null);
 
   useEffect(() => {
+    // 'WebSocket' 인스턴스를 생성하여 'url'로 연결을 시도
     const socket = new WebSocket(url);
 
+    // 'jsmpeg' 플레이어를 생성하여 'playerRef'에 저장
+    // 'socket'과 'canvasRef.current'를 인자로 전달하여
+    // 웹소켓을 통한 스트리밍과 캔버스에 동영상을 그리는 작업을 연결
     playerRef.current = new jsmpeg(socket, {
       canvas: canvasRef.current,
     });
